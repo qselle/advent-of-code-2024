0a446e65afb5d453c9fd5779ef8d1e5ea96bc7dc
Quentin Selle <62110608+quentinselle@users.noreply.github.com>
2024-12-13 00:01:44 +0100

    Add day10

--- src/day10.rs
+++ src/day10.rs
@@ -1,82 +1,74 @@
+use std::collections::HashSet;
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day10)]
 pub fn input_generator(input: &str) -> Vec<Vec<usize>> {
+    let mut starts = vec![];
     input
         .lines()
-        .map(|s| {
+        .enumerate()
+        .map(|(y, s)| {
             s.chars()
-                .map(|l| l.to_digit(10).unwrap() as usize)
+            .enumerate()
+                .map(|(x, l)| {
+                    if l == '0' { 
+                        starts.push((y,x))
+                    } l.to_digit(10).unwrap() as usize
+        })
                 .collect()
         })
         .collect()
 }
 
-// #[memoize]
-// pub fn blink(stone: usize, counter: usize, limit: usize) -> usize {
-//     let mut stack = 0;
-//     if counter == limit {
-//         return 1;
-//     }
+pub fn hike(input: &[Vec<usize>], pos: (usize, usize), current: usize, visited: &mut HashSet<(usize,usize)>) -> usize {
+    if current == 9 {
+        if !visited.contains(&(pos.0, pos.1)) {
+            visited.insert((pos.0, pos.1));
+            return 1;
+        }
+        return 0;
+    }
+    let mut path = 0;
 
-//     if stone == 0 {
-//         stack += blink(1, counter + 1, limit);
-//     } else {
-//         let mut div = stone;
-//         let mut count = 0;
-//         while div > 0 {
-//             div /= 10;
-//             count += 1;
-//         }
-//         if count % 2 == 0 {
-//             stack += blink(stone / 10_usize.pow(count / 2), counter + 1, limit);
-//             stack += blink(stone % 10_usize.pow(count / 2), counter + 1, limit);
-//         } else {
-//             stack += blink(stone * 2024, counter + 1, limit);
-//         }
-//     }
-//     stack
-// }
+    let directions = [
+        (0, -1), // left,
+        (0, 1), // right,
+        (-1, 0), // up,
+        (1, 0), // down,
+    ];
+    
+    for (dy, dx) in directions {
+        let ny = pos.0 as isize + dy;
+        let nx = pos.1 as isize + dx;
+        if ny >= 0
+        && ny < input.len() as isize
+        && nx >= 0
+        && nx < input[pos.0].len() as isize
+        && input[ny as usize][nx as usize] == current + 1 {
+            path += hike(input, (ny as usize, nx as usize), current+1, visited);
+        }
+
+    }
+    path
+}
 
 #[aoc(day10, part1)]
 pub fn part1(input: &[Vec<usize>]) -> usize {
-    // let directions = [
-    //     (0, 1),  // right
-    //     (0, -1), // left
-    //     (1, 0),  // up
-    //     (-1, 0), // down
-    // ];
-    // for (row_idx, row) in input.iter().enumerate() {
-    //     dbg!(row_idx, row);
-    //     for (col_idx, col) in row.iter().enumerate() {
-    //         dbg!(col_idx, col);
-    //         if *col == 0 {
-    //             for &(dy, dx) in &directions {
-    //                 let ny: isize = y as isize + i * dy;
-    //                 let nx = x as isize + i * dx;
-    //                 if ny >= 0
-    //                     && ny < input.len() as isize
-    //                     && nx >= 0
-    //                     && nx < input[y].len() as isize
-    //                 {
-    //                     match *col {}
-    //                 }
-    //             }
-    //             // match input[row_idx][col_idx + 1]
-    //         }
-    //     }
-    // }
-    0
+    let mut score = 0;
+    let mut count = 0;
+    for (y, row) in input.iter().enumerate() {
+        for (x, col) in row.iter().enumerate() {
+            if *col == 0 {
+                let mut visited: HashSet<(usize,usize)> = HashSet::new();
+                score += hike(input, (y, x), 0, &mut visited);    
+                count +=1;
+                dbg!(count, score);
+            }
+        }
+    }
+    dbg!(count, score);
+    score
 }
-
-// #[aoc(day10, part2)]
-// pub fn part2(input: &[usize]) -> usize {
-//     let mut lenght = 0;
-//     for stone in input {
-//         lenght += blink(*stone, 0, 75);
-//     }
-//     lenght
-// }
 
 #[cfg(test)]
 mod tests {
@@ -93,11 +85,6 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(55312, part1(&input_generator(INPUT)))
+        assert_eq!(36, part1(&input_generator(INPUT)))
     }
-
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(55312, part2(&input_generator(INPUT)))
-    // }
 }
