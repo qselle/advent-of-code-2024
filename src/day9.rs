7df8d449c336d048204d5ba50806caa8b9b0c754
Quentin Selle <62110608+quentinselle@users.noreply.github.com>
2024-12-16 01:04:35 +0100

    Add day9 part1 - working

--- src/day9.rs
+++ src/day9.rs
@@ -28,61 +28,79 @@ pub fn input_generator(input: &str) -> Vec<Disk> {
 
 #[aoc(day9, part1)]
 pub fn part1(input: &[Disk]) -> usize {
+    let mut i = 0;
+    let mut input = input.to_vec();
+
+    loop {
+        // for j in &input {
+        //     match j {
+        //         Disk::File { id, blocks } => {
+        //             print!("{}", id.to_string().repeat(*blocks));
+        //         }
+        //         Disk::Free { blocks } => {
+        //             print!("{}", ".".repeat(*blocks));
+        //         }
+        //     }
+        // }
+        // println!();
+
+        match input[i] {
+            Disk::File { id: _, blocks: _ } => {
+                i += 1;
+            }
+            Disk::Free {
+                blocks: free_blocks,
+            } => {
+                if let Some(last) = input.pop() {
+                    match last {
+                        Disk::Free { blocks: _ } => continue,
+                        Disk::File { id, blocks } => match blocks.cmp(&free_blocks) {
+                            std::cmp::Ordering::Equal => {
+                                input[i] = last;
+                                i += 1;
+                            }
+                            std::cmp::Ordering::Less => {
+                                input.insert(i, last);
+                                input[i + 1] = Disk::Free {
+                                    blocks: free_blocks - blocks,
+                                };
+                                i += 1;
+                            }
+                            std::cmp::Ordering::Greater => {
+                                input[i] = Disk::File {
+                                    id,
+                                    blocks: free_blocks,
+                                };
+                                input.push(Disk::File {
+                                    id,
+                                    blocks: blocks - free_blocks,
+                                });
+                                i += 1;
+                            }
+                        },
+                    }
+                }
+            }
+        }
+        if i >= input.len() {
+            break;
+        }
+    }
+
+    let mut res = 0;
+    let mut pos = 0;
     for i in input {
         match i {
             Disk::File { id, blocks } => {
-                print!("{}", id.to_string().repeat(*blocks));
-            }
-            Disk::Free { blocks } => {
-                print!("{}", ".".repeat(*blocks));
-            }
-        }
-    }
-    println!();
-
-    let mut i = 0;
-    let mut input = input.to_vec();
-    let mut results: Vec<usize> = Vec::new();
-
-    loop {
-        if i > input.len() {
-            break;
-        }
-
-        match input[i] {
-            Disk::File { id, blocks } => results.extend(std::iter::repeat(id).take(blocks)),
-            Disk::Free {
-                blocks: free_blocks,
-            } => loop {
-                if let Some(last) = input.pop() {
-                    match last {
-                        Disk::Free { blocks } => continue,
-                        Disk::File { id: _, blocks } => {
-                            if blocks == *free_blocks {
-                                results.push(last);
-                            }
-                        }
-                    }
-                } else {
-                    break;
+                for _ in 0..blocks {
+                    res += pos * id;
+                    pos += 1;
                 }
-            },
+            }
+            _ => unreachable!(),
         }
     }
-
-    for i in results {
-        match i {
-            Disk::File { id, blocks } => {
-                print!("{}", id.to_string().repeat(blocks));
-            }
-            Disk::Free { blocks } => {
-                print!("{}", ".".repeat(blocks));
-            }
-        }
-    }
-    println!();
-
-    0
+    res
 }
 
 // #[aoc(day9, part2)]
@@ -99,7 +117,7 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(1, part1(&input_generator(INPUT)))
+        assert_eq!(1928, part1(&input_generator(INPUT)))
     }
 
     // #[test]
