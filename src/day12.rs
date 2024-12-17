f07051ed0a2a4f27c2c61acac614c3766339a9d3
Quentin Selle <62110608+quentinselle@users.noreply.github.com>
2024-12-17 23:59:59 +0100

    WIP day12

--- src/day12.rs
+++ src/day12.rs
@@ -1,10 +1,42 @@
-// use aoc_runner_derive::{aoc, aoc_generator};
+use aoc_runner_derive::{aoc, aoc_generator};
 
-// #[aoc_generator(day8)]
-// pub fn input_generator(input: &str) -> (Vec<usize>, Vec<usize>) {
+#[aoc_generator(day12)]
+pub fn input_generator(input: &str) -> Vec<Vec<char>> {
+    input.lines().map(|s: &str| s.chars().collect()).collect()
+}
 
-// #[aoc(day8, part1)]
-// pub fn part1(input: &[Equation]) -> usize {
+#[aoc(day12, part1)]
+pub fn part1(_input: &[Vec<char>]) -> usize {
+    0
+}
 
-// #[aoc(day8, part2)]
-// pub fn part2(input: &[Equation]) -> usize {
+// #[aoc(day12, part2)]
+// pub fn part2(_input: &[Vec<char>]) -> usize {
+//     0
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(1930, part1(&input_generator(INPUT)))
+    }
+
+    // #[test]
+    // fn test_part2() {
+    //     assert_eq!(65601038650482, part2(&input_generator(INPUT)))
+    // }
+}
